2e58d8249f8abe37674708d75582f98df4a79e1a
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-05-29 16:14:53 -0400

    modified job name in run_train_hard.sh

--- shell_command/run_train_hard.sh
+++ shell_command/run_train_hard.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=guest-gpu
 #SBATCH --qos=low-gpu
 #SBATCH --time=24:00:00
-#SBATCH --job-name=train-uniform
+#SBATCH --job-name=train-hard
 #SBATCH --mail-user=zli@brandeis.edu
 #SBATCH --output=shell_command/output/%j.txt
 #SBATCH --nodes=1

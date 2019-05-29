600702c6e79b638b40ca86548233e3881af8ef1a
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-05-29 16:12:10 -0400

    added script for running train_hard.py in shell command

---
+++ shell_command/run_train_hard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=guest
+#SBATCH --partition=guest-gpu
+#SBATCH --qos=low-gpu
+#SBATCH --time=24:00:00
+#SBATCH --job-name=train-uniform
+#SBATCH --mail-user=zli@brandeis.edu
+#SBATCH --output=shell_command/output/%j.txt
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:TitanXP:1
+
+python3 train_hard.py

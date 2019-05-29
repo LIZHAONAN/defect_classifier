0d1a51926fb04858df192e2db2c46d7fc527acfa
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-05-29 12:33:46 -0400

    add shell command folder

---
+++ shell_command/run_train_uniform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=pengyu-lab
+#SBATCH --partition=pengyu-gpu
+#SBATCH --time=00:10:00
+#SBATCH --job-name=train-uniform
+#SBATCH --mail-user=zli@brandeis.edu
+#SBATCH --output=output_%j.txt
+#SBATCH --node=2
+
+cd ..
+python3 train_uniform.py

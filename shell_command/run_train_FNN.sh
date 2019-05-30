05d1801e5ab6ab9090f62dc59e6ac4bb70d76415
Zhaonan <32943976+LIZHAONAN@users.noreply.github.com>
2019-05-30 11:15:39 -0400

    added script for trainning FNN

---
+++ shell_command/run_train_FNN.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=guest
+#SBATCH --partition=guest-gpu
+#SBATCH --qos=low-gpu
+#SBATCH --time=24:00:00
+#SBATCH --job-name=train-FNN
+#SBATCH --mail-user=zli@brandeis.edu
+#SBATCH --output=shell_command/output/%j.txt
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:GTX:2
+
+python3 train_FNN.py
